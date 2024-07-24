c40c4366ac3fea4096420f336a7c2e6e5f865e45
rathtina <156821818+rathtina@users.noreply.github.com>
2024-07-24 23:53:13 +0700

    Add files via upload

---
+++ CafemanagementApplication.java
@@ -0,0 +1,13 @@
+package org.hometesting.cafemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafemanagementApplication.class, args);
+    }
+
+}
